e3f63bf0305688ee4f583ed97d0d2f2143081a59
终极天谴怪 <1094410998@qq.com>
2023-04-29 18:32:58 +0800

    为get_url添加爬取所有商品的功能 同时测试第一版steam差价计算

--- linux_arm/get_url.py
+++ linux_arm/get_url.py
@@ -27,3 +27,22 @@ while True:
     time.sleep(10)
     index = index + 1
 
+driver.get('https://buff.163.com/market/csgo#tab=selling&page_num=1')
+time.sleep(100)
+index = 2
+while True:
+    element = driver.find_element(By.ID, "j_market_card")
+    elements = element.find_elements(By.TAG_NAME, "li")
+    for li in elements:
+        try:
+            url = li.find_element(By.TAG_NAME, "a").get_attribute("href").split("?")[0]
+            if 'page' not in url:
+                print(url)
+        except Exception:
+            time.sleep(1)
+    driver.get('https://buff.163.com/market/csgo#tab=selling&page_num=' + str(index))
+    driver.get('https://buff.163.com/market/csgo#tab=selling&page_num=' + str(index))
+
+    time.sleep(10)
+    index = index + 1
+#获取所有的url
---
+++ windows/steam_buff_price_calculate.py
@@ -0,0 +1,38 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=Service(r'webdriver\chromedriver.exe'))
+
+with open('../all.txt') as f:
+    urls = f.readlines()
+difference = {}
+with open('../all.txt', 'w') as f:
+    for url in urls:
+        driver.get(url)
+        time.sleep(3)
+        while True:
+            price_elements = driver.find_elements(By.CLASS_NAME, "f_Strong")
+            on_sale_count = driver.find_element(By.CLASS_NAME, "new-tab").text.split("Sell")[1].split(")")[0].replace(
+                "(", "").replace("+", "")
+            if int(on_sale_count) < 100:
+                break
+            if len(price_elements) > 1:
+                steam_price = price_elements[0]
+                lowest_price = price_elements[1]
+                if float(lowest_price.text.replace("¥ ", "")) > 100:
+                    break
+                different = float(lowest_price.text.replace("¥ ", "")) - float(
+                    steam_price.text.replace("¥ ", "")) / 0.15
+                difference[url] = different
+                break
+        time.sleep(5)
+the_best_url = ''
+price_difference = 0
+for di in difference:
+    if difference[di] > price_difference:
+        the_best_url = di
+        price_difference = difference[di]
+print(the_best_url + ":" + str(price_difference))
