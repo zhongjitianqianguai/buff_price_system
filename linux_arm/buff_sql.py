2fcfac0d49362a4f708eb9ba2c69b0b816fbd4be
终极天谴怪 <1094410998@qq.com>
2023-10-30 10:28:11 +0800

    新增从csob爬取

--- linux_arm/buff_sql.py
+++ linux_arm/buff_sql.py
@@ -129,7 +129,7 @@ def get_user_mail_by_user_id(goods_id):
     conn = pool.connection()
     cursor = conn.cursor()
     try:
-        sql = """Select mail from  buff_user where user_id=%s;"""
+        sql = """Select email from  buff_user where user_id=%s;"""
         conn.ping(reconnect=True)
         cursor.execute(sql, goods_id)  # 添加参数
         temp = cursor.fetchall()
